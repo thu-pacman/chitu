7b3b8028b84eb5336f043e81a601a823fd877833
唐适之 <tangshizhi@chitu.ai>
2025-03-21 09:41:40 +0000

    Add QwQ-32B

--- test/test_benchmarking_tool.sh
+++ test/test_benchmarking_tool.sh
@@ -2,8 +2,8 @@
 
 trap 'pkill -P $(jobs -p)' EXIT
 
-grun torchrun --nproc_per_node 1 --master_port=22528 -m chitu models=Qwen2-7B-Instruct models.ckpt_dir=/home/share/models/Qwen2-7B-Instruct serve.host=127.0.0.1 serve.port=21100 &
-sleep 10s
+grun torchrun --nproc_per_node 1 --master_port=22513 -m chitu models=Qwen2-7B-Instruct models.ckpt_dir=/home/share/models/Qwen2-7B-Instruct serve.host=127.0.0.1 serve.port=21100 &
+sleep 20s
 python benchmarks/benchmark_serving.py --model "qwen2-7b" --iterations 10 --seq-len 10 --warmup 3 --base-url http://127.0.0.1:21100
 echo "Testing done. You may see some following killing messages, which is expected."
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
