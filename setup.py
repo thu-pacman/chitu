42b7a863f9200a2b9ce11303aa86611107d077d5
lijian <lijian@chitu.ai>
2025-04-22 13:37:41 +0000

    doc: add cpu+moe readme

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ if os.environ.get("CHITU_WITH_CYTHON", "0") != "0":
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="chitu",
-    version="0.2.1",
+    version="0.2.2",
     install_requires=[
         # Don't put `torch` here because it requires downloading from a specific source
         "transformers<4.47",  # Required by auto_gptq
