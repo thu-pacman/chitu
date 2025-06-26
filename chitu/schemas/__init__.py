a4cd8549263b673baa55c32d9fbe50ef6ac536c6
杨金舜 <yangjinshun@chitu.ai>
2025-06-26 07:11:32 +0000

    Add enumeration type check and rule check with Hydra

--- chitu/schemas/__init__.py
+++ chitu/schemas/__init__.py
@@ -1,9 +1,8 @@
 from hydra.core.config_store import ConfigStore
 from chitu.schemas.serve_config import ServeConfig
+from chitu.schemas.serve_config_rules import ServeConfigRules
 
 cs = ConfigStore.instance()
 cs.store(name="serve_config_schema", node=ServeConfig)
 
-__all__ = [
-    "ServeConfig",
-]
+__all__ = ["ServeConfig", "ServeConfigRules"]
---
+++ chitu/schemas/serve_config_rules.py
@@ -0,0 +1,78 @@
+from hydra.experimental.callback import Callback
+from omegaconf import DictConfig
+import sys
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
+class ServeConfigRules(Callback):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def _exit_with_error(self, message):
+        """Fatal error, exit method"""
+        logger.error(f"Config Error: {message}")
+        sys.exit(1)
+
+    def on_job_start(self, config: DictConfig, **kwargs) -> None:
+        port = config.serve.port
+        if not (1024 <= port <= 65535):
+            self._exit_with_error(f"Port must be between 1024 and 65535, got {port}")
+
+        num_blocks = config.infer.num_blocks
+        if num_blocks < 0 and num_blocks != -1:
+            self._exit_with_error(
+                f"num_blocks must be positive or -1 (got {num_blocks})"
+            )
+
+        attn_type = config.infer.attn_type
+        if attn_type == "npu":
+            try:
+                import torch_npu
+            except ImportError:
+                self._exit_with_error(
+                    f"torch-npu required for attn_type=npu (got {attn_type})"
+                )
+        if attn_type not in {
+            "flash_attn",
+            "flash_mla",
+            "flash_infer",
+            "triton",
+            "npu",
+            "ref",
+        }:
+            self._exit_with_error(
+                f"attn_type must be one of [flash_attn, flash_mla, flash_infer, triton, npu, ref], got {attn_type}"
+            )
+
+        op_impl = config.infer.op_impl
+        if op_impl not in {"torch", "muxi_custom_kernel"}:
+            self._exit_with_error(
+                f"op_impl must be one of [torch, muxi_custom_kernel], got {op_impl}"
+            )
+
+        bind_process_to_cpu = config.infer.bind_process_to_cpu
+        if bind_process_to_cpu not in {"auto", "none", "numa"}:
+            self._exit_with_error(
+                f"bind_process_to_cpu must be one of [auto, none, numa], got {bind_process_to_cpu}"
+            )
+
+        bind_thread_to_cpu = config.infer.bind_thread_to_cpu
+        if bind_thread_to_cpu not in {"physical_core", "logical_core"}:
+            self._exit_with_error(
+                f"bind_thread_to_cpu must be one of [physical_core, logical_core], got {bind_thread_to_cpu}"
+            )
+
+        scheduler_type = config.scheduler.type
+        if scheduler_type not in {
+            "fcfs",
+            "prefill_first",
+            "stride",
+            "deadline",
+            "prefix_align",
+            "balance",
+        }:
+            self._exit_with_error(
+                f"scheduler type must be one of [fcfs, prefill_first, stride, deadline, prefix_align, balance], got {scheduler_type}"
+            )
