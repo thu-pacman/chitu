febf69a4f5c06956c5990c249396a79e1784c527
唐适之 <tangshizhi@chitu.ai>
2025-09-15 15:14:07 +0800

    Remove some constraints on dependencies

--- get_requires.py
+++ get_requires.py
@@ -20,7 +20,7 @@ install_requires = [
     #    torch back to the official version, please use `-c` on `pip`.
     "torch",
     "transformers[torch]",
-    "safetensors<0.6",  # 0.6 breaks on muxi
+    "safetensors",
     "fire",
     "tiktoken>=0.7.0",  # Required by glm4
     "blobfile",
@@ -29,7 +29,7 @@ install_requires = [
     "fastapi",
     "uvicorn[standard]",
     "tqdm",
-    "accelerate<1.10",  # 1.10 breaks on muxi
+    "accelerate",
     "einops",
     "typing-extensions",
     "pyzmq>=27.0.0",
