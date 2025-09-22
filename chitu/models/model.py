9770bbe7969d6b28e5dd920c59559369886e3321
张运宸 <zhangyunchen@chitu.ai>
2025-09-22 14:17:48 +0000

    quant(ascend-int8) support ascend int8 models with layernorm bias

--- chitu/models/model.py
+++ chitu/models/model.py
@@ -102,6 +102,27 @@ class RMSNorm(nn.Module):
         )
 
 
+class RMSNormBias(RMSNorm):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__(dim=dim, eps=eps)
+        self.bias = nn.Parameter(
+            torch.zeros(self.dim, dtype=torch.get_default_dtype()), requires_grad=False
+        )
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        out: Optional[torch.Tensor] = None,
+        compute_dtype=None,
+        impl: str = "auto",
+    ):
+        return (
+            super()
+            .forward(x, out, compute_dtype if compute_dtype else torch.float32, impl)
+            .add_(self.bias)
+        )
+
+
 class Attention(nn.Module):
     def __init__(self, layer_id, cache, attn_backend):
         super().__init__()
@@ -999,3 +1020,7 @@ def get_linear_layout_contig_y(
         return QuantizationRegistry.get_quantized_linear_class_from_global_args(
             quant_kwargs=quant_kwargs, checkpoint_prefix=checkpoint_prefix
         )
+
+
+def get_rmsnorm(dim: int, *, use_bias: bool, eps: float = 1e-6) -> RMSNorm:
+    return RMSNormBias(dim, eps=eps) if use_bias else RMSNorm(dim, eps=eps)
