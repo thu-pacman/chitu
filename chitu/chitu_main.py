b37cb844f0ef758b686b39150b1abf3d6ba77bfb
杨启龙 <yangqilong@chitu.ai>
2025-09-30 12:13:49 +0800

    The DeepSeek-V3 type model disables CUDA graphs when NPU and paged are enabled.

--- chitu/chitu_main.py
+++ chitu/chitu_main.py
@@ -435,6 +435,12 @@ def chitu_init(args, logging_level=None):
             args.infer.use_cuda_graph = False
         elif args.models is not None and str(args.models).find("'type': 'mixq'") != -1:
             args.infer.use_cuda_graph = False
+        elif (
+            args.infer.attn_type == "npu"
+            and args.infer.cache_type == "paged"
+            and (args.models.type is not None and args.models.type == "deepseek-v3")
+        ):
+            args.infer.use_cuda_graph = False
         else:
             args.infer.use_cuda_graph = True
 
