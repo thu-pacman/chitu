6d3b65c2ec4419557d8ff7c66a96cee94b6632b6
唐适之 <tangshizhi@chitu.ai>
2026-02-06 09:22:51 +0800

    Bump to 0.5.1

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ if os.environ.get("CHITU_SETUP_JOBS") is not None:
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="chitu",
-    version="0.5.0",
+    version="0.5.1",
     python_requires=">=3.10",
     install_requires=install_requires,
     extras_require=extras_require,
