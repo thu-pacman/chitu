de854aa2030b0659e032d59c54536cec3e33b66e
Chengzhang <wcz24@mails.tsinghua.edu.cn>
2026-03-12 14:13:18 +0800

    minor solve conflict.

--- chitu/task.py
+++ chitu/task.py
@@ -22,11 +22,7 @@ from typing_extensions import override
 
 import torch
 
-<<<<<<< HEAD
 from chitu.task_type import TaskType, TaskDecodeType, is_prefill, is_decode
-=======
-from chitu.task_type import TaskType
->>>>>>> public-main
 from chitu.async_response import AsyncDataStream
 from chitu.backend import Backend
 from chitu.device_list import DeviceList, StaticDeviceListManager
