bf7051dbebe0a7ee7fd050d654e68609ae1c5cd1
唐适之 <tangshizhi@chitu.ai>
2026-03-16 14:35:07 +0000

    Revert "Merge branch 'feature/transfer-multimodal-cache-to-kv-cache' into 'main'"

--- chitu/chitu_main.py
+++ chitu/chitu_main.py
@@ -376,7 +376,7 @@ def _warmup_backend_direct(
         )
     seq_len_list = [1] * local_max_bs
     # Prefill
-    for mgn, mgr in Backend.cache_managers.items():
+    for mgr in Backend.cache_managers.values():
         mgr.prepare_cache_prefill(req_ids, seq_len_list)
     PrometheusMetricsCollector.update_kvcache_usage()
 
@@ -400,9 +400,8 @@ def _warmup_backend_direct(
         ):
             curr_bs = local_max_bs - i * bs_descend
             curr_req_ids = req_ids[:curr_bs]
-            for mgn, mgr in Backend.cache_managers.items():
-                if mgn != "multimodal":
-                    mgr.prepare_cache_decode(curr_req_ids)
+            for mgr in Backend.cache_managers.values():
+                mgr.prepare_cache_decode(curr_req_ids)
             PrometheusMetricsCollector.update_kvcache_usage()
 
             # direct warmup 绕过了 executor，因此必须在这里显式设置
